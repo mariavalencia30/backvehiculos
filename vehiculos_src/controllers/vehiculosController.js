0100ffb17f252c19b8468de8584750dc2c70cf09
Maria Valencia <mariavalenciah30@gmail.com>
2025-04-05 14:43:49 -0500

    Fix error message in buscarVehiculosPorNombre to be singular

--- vehiculos_src/controllers/vehiculosController.js
+++ vehiculos_src/controllers/vehiculosController.js
@@ -79,7 +79,7 @@ const buscarVehiculosPorNombre = async (req, res) => {
         const vehiculos = await Vehiculo.buscarPorNombre(nombre);
         res.status(200).json(vehiculos);
     } catch (err) {
-        res.status(500).json({ message: "Error al buscar vehículos por nombre", error: err });
+        res.status(500).json({ message: "Error al buscar vehículo por nombre", error: err });
     }
 };
 
